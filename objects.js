31e623a12d2f03cd5f54202b9a93919b9749d03a
shubha <shubha07658@gmail.com>
2021-10-25 21:42:33 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,11 @@
+var name = "shubha"
+var studentData = {
+    Roll_Number: true,
+    Name: "Gullu",
+    Branch: "ECE"
+}
+
+console.log(name)
+console.log(studentData.Roll_Number)
+
+console.log(typeof(studentData))
